4dfa2c379873e929dd3eee7bd85975a7df26ffc7
almatkai <kairatovalmat2003@gmail.com>
2024-11-13 18:04:50 +0500

    structure

---
+++ internal/entity/user.go
@@ -0,0 +1,13 @@
+// internal/entity/user.go
+package entity
+
+import "time"
+
+type User struct {
+	ID           string    `json:"id"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
